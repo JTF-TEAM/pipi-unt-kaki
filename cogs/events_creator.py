7191e029a54562862272c8b0413f809c2e46c611
asd123 <123@123.com>
2022-10-15 20:05:55 +0300

    events

--- cogs/events_creator.py
+++ cogs/events_creator.py
@@ -10,22 +10,25 @@ class eventcreator(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.guilds = [guild.id for guild in bot.guilds]
+        # set before deploy
+        self.event_channel_id = 0 
+        self.event_role_id = 0
 
     
     @nextcord.slash_command()
-    async def create_event(self, interaction: nextcord.Interaction, event_name: str, event_description: str):
-        # await interaction.guild.create_scheduled_event(
-        #     name=event_name, 
-        #     description=event_description,
-        #     end_time= datetime.datetime.now(),
-        #     start_time=datetime.datetime.now(),
-        #     entity_type= nextcord.ScheduledEventEntityType.external
-        #     )
-        return
+    async def create_event(self, interaction: nextcord.Interaction, event_name: str, event_description: str, event_cash: int, date:str, time:str):
+        event_channel = interaction.guild.get_channel(self.event_channel_id)
+        event_role = interaction.guild.get_role(self.event_role_id)
+        if event_role != None and event_channel != None:
+            E = nextcord.Embed(
+                title=f"{event_name} ивент",
+                description=f"{event_description}\nДеняк за эвент: {event_cash} $\nвремя проведения: {date} {time}"
+            )
 
-    @nextcord.slash_command()
-    async def set_event_pic(self, interaction: nextcord.Interaction, event_name: str, event_description: str):
-        return
+            await interaction.response.send_message(contern = event_role.mention ,embed=E)
+        else:
+            await interaction.response.send_message('что то пошло не так')
+
     
 
 def setup(bot):
