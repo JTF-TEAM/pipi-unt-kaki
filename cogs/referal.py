31729a9ceb5424c5e1711aa52523f008e7305ce8
asd123 <123@123.com>
2022-10-26 15:22:51 +0300

    referal kinda sus

--- cogs/referal.py
+++ cogs/referal.py
@@ -9,13 +9,23 @@ class ref(commands.Cog):
 
     
     @nextcord.slash_command(name='create_referal')
-    async def asd(self, interaction: nextcord.Interaction):
+    async def asd(self, interaction: nextcord.Interaction, description:str):
         bukvi = 'qweasdzxcvbnfghrtyuiojklmp0123456789PLOKMIJNUHBYGVTFCRDXESZWAQ'
         dlinna = len(bukvi)-1
         ssilka = ''
         for i in range(11):
             ssilka += bukvi[random.randint(0, dlinna)]
-        self.databasa.insert_one({'guild':interaction.guild, 'ssilka':ssilka})
+        self.databasa.insert_one(
+            {
+                'guild_id':interaction.guild, 
+                'ssilka':ssilka,
+                'creator_id':interaction.user.id,
+                'uses': 0,
+                'active': True,
+                'description': description,
+                'users':[]
+            }
+        )
 
         await interaction.response.send_message(f'ВАШ РЕФЕРАЛЪ {ssilka}')
         return
@@ -24,13 +34,51 @@ class ref(commands.Cog):
     async def asd(self, interaction: nextcord.Interaction, ssilka:str):
         est = self.databasa.find_one({'guild':interaction.guild, 'ssilka':ssilka})
         if est != None:
-            self.databasa.delete_one({'guild':interaction.guild, 'ssilka':ssilka})
-            nihuya = 'много недег'
-            await interaction.response.send_message(f'поздравляем вы успешно активировали ссылку и получили {nihuya}')
+            if est['active'] == True:
+                if interaction.user.id in est['users']:
+                    await interaction.response.send_message('вы уже использовали эту ссылку')
+                else:
+                    new_uses = est['uses']+1
+                    new_users = est['users'].copy()
+                    self.databasa.update_one({'ssilka': ssilka}, {'$set': {'uses':new_uses, 'users':new_users.append(interaction.user.id)}})
+                    nihuya = est['description']
+                    await interaction.response.send_message(f'поздравляем вы успешно активировали ссылку и получили {nihuya}')
+            else:
+                await interaction.response.send_message(f'Ссылка в данный момент не активна')
         else:
             await interaction.response.send_message(f'таких ссыылок нет')
         return
 
+    @nextcord.slash_command(name='edit_referal')
+    async def asd(self, interaction: nextcord.Interaction, ssilka:str,  mode:str):
+        est = self.databasa.find_one({'guild':interaction.guild, 'ssilka':ssilka})
+        if est != None:
+            if mode == 'activate':
+                self.databasa.update_one({'ssilka': ssilka}, {'$set': {'active':True}})
+                await interaction.response.send_message('ссылка активирована')
+            elif mode == 'deactive':
+                self.databasa.update_one({'ssilka': ssilka}, {'$set': {'active':False}})
+                await interaction.response.send_message('ссылка дизактивирована')
+            else:
+                await interaction.response.send_message(f'данная команда используется по примеру - ssilka(vasha ssilka), mode(active/deactive)')
+        else:
+            await interaction.response.send_message('такой ссылки нету')
+    
+    @nextcord.slash_command(name='referal_info')
+    async def asd(self, interaction: nextcord.Interaction, ssilka:str):
+        est = self.databasa.find_one({'guild':interaction.guild, 'ssilka':ssilka})
+        if est != None:
+            users = ''
+            for aidi in est['users']:
+                users += f"<@{aidi}>\n"
+            await interaction.response.send_message(
+                embed=nextcord.Embed(
+                    title='fer info',
+                    description=f"автор: <@{est['creator_id']}>\nиспользований {est['uses']}\nstatus {est['active']}\nmembers: {users}"
+                )
+            )
+        else:
+            await interaction.response.send_message('такой ссылки нету')
 
 def setup(bot):
     bot.add_cog(ref(bot))
